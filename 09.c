a5125fac9e64b94599a4b212600abbb31d58b78d
vivekbharadwaj009 <140109492+vivekbharadwaj009@users.noreply.github.com>
2025-02-12 18:59:26 +0530

    Update 09.c

--- 09.c
+++ 09.c
@@ -2,39 +2,40 @@
 #include <string.h>
 
 int main() {
-    int count, src_router, i, j, k, w, v, min;
+    int n, srcRouter, i, j, k, w, v, min;
     
-    int cost_matrix[100][100], dist[100], last[100];
-    int flag[100];
+    int costMatrix[100][100], dist[100], last[100];
+    int vis[100];
     
-    printf("\n Enter the no of routers");
-    scanf("%d",&count);
+    printf("Enter the no of routers : \n");
+    scanf("%d", &n);
     
-    printf("\n Enter the cost matrix values:");
-    for(i = 0;i < count; i++) {
-        for(j = 0;j < count; j++){
-            printf("\n%d->%d:",i,j);
-            scanf("%d",&cost_matrix[i][j]);
-            if(cost_matrix[i][j] < 0) {
-                cost_matrix[i][j] = 1000;
+    printf("Enter the cost matrix values : \n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++){
+            printf("\n%d -> %d : ", i, j);
+            scanf("%d", &costMatrix[i][j]);
+            if(costMatrix[i][j] < 0) {
+                costMatrix[i][j] = 1000;
             }
         }
     }
     
-    printf("\n Enter the source router:");
-    scanf("%d",&src_router);
+    printf("\n");
+    printf("Enter the source router: \n");
+    scanf("%d", &srcRouter);
     
-    for(v = 0; v < count; v++) {
-        flag[v] = 0;
-        last[v] = src_router;
-        dist[v] = cost_matrix[src_router][v];
+    for(v = 0; v < n; v++) {
+        vis[v] = 0;
+        last[v] = srcRouter;
+        dist[v] = costMatrix[srcRouter][v];
     }
         
-    flag[src_router] = 1;
-    for(i = 0; i < count; i++) {
+    vis[srcRouter] = 1;
+    for(i = 0; i < n; i++) {
         min = 1000;
-        for(w = 0; w < count; w++) {
-            if(!flag[w]) {
+        for(w = 0; w < n; w++) {
+            if(!vis[w]) {
                 if(dist[w] < min) {
                     v = w;
                     min = dist[w];
@@ -42,24 +43,24 @@ int main() {
             }
         }
         
-        flag[v] = 1;
-        for(w = 0; w < count; w++) {
-            if(!flag[w]) {
-                if(min + cost_matrix[v][w] < dist[w]) {
-                    dist[w] = min + cost_matrix[v][w];
+        vis[v] = 1;
+        for(w = 0; w < n; w++) {
+            if(!vis[w]) {
+                if(min + costMatrix[v][w] < dist[w]) {
+                    dist[w] = min + costMatrix[v][w];
                     last[w] = v;
                 }
             }
         }
     }
     
-    for(i = 0; i < count; i++) {
-        printf("\n%d==>%d: Path taken:%d",src_router,i,i);
+    for(i = 0; i < n; i++) {
+        printf("%d ==> %d : Path taken : %d", srcRouter, i, i);
         w = i;
-        while(w != src_router) {
-            printf("\n<--%d",last[w]);
+        while(w != srcRouter) {
+            printf("\n<-- %d",last[w]);
             w = last[w];
         }
-        printf("\n Shortest path cost:%d",dist[i]);
+        printf("\nShortest path cost : %d\n",dist[i]);
     }
 }
